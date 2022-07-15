36e40bc1263411fba03c49d604876cfabf92b140
Katelyn <katelyngovender22@gmail.com>
2022-07-15 15:54:22 +0200

    Form colors updated and dropdown bug fixed

--- src/app/shared/global-constants.ts
+++ src/app/shared/global-constants.ts
@@ -1,9 +1,9 @@
 export class GlobalConstants {
-    public static pageTitleStyle: string = "linear-gradient(147deg, #eff3f7 72%, #228b22 28%)";
+    public static pageTitleStyle: string = "linear-gradient(147deg, #eff3f7 72%, #964400  28%)";
 
-    public static SectionStyle: string = "linear-gradient(147deg, #eff3f7 72%, #009e60 28%)";
+    public static SectionStyle: string = "linear-gradient(147deg, #eff3f7 72%, #bf7c00 28%)";
 
-    public static SubsectionStyle: string = "linear-gradient(147deg, #eff3f7 72%, #50c878 28%)";
+    public static SubsectionStyle: string = "linear-gradient(147deg, #eff3f7 72%, #cca372 28%)";
 
-    public static groupStyle: string = "linear-gradient(147deg, #eff3f7 72%, #77dd77 28%)";
+    public static groupStyle: string = "linear-gradient(147deg, #eff3f7 72%, #e8ccb0  28%)";
 }
