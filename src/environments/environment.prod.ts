9c9314c8d8fb9ef0788ffc5739653ad13125f327
MpiloMsomi <95614874+MpiloMsomi@users.noreply.github.com>
2023-10-18 09:43:05 +0200

    Environment Changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,18 +3,18 @@ export const environment = {
   // API_URL: 'https://testportal.dsd.gov.za/MNE_DSD_API/api',
   // API_FormURL: 'https://testportal.dsd.gov.za/MNE_DSD_API/1/',
   // API_FormURLForHelp: 'https://testportal.dsd.gov.za/MNE_DSD_API',
-  
-  API_URL: 'https://app1.terra.group/MNE_UAT_API/api',
-  API_FormURL: 'https://app1.terra.group/MNE_UAT_API/1/'  ,
-  API_FormURLForHelp: 'https://app1.terra.group/dsd_uat',
 
-  REPORT_SERVER: 'https://app1.terra.group/DSDReportViewerSSRS/Default?ID=',
+  API_URL: 'https://app1.terra.group/MNE_PHASE2_API/api',
+  API_FormURL: 'https://app1.terra.group/MNE_PHASE2_API/1/',
+  API_FormURLForHelp: 'https://app1.terra.group/MNE_PHASE2_API',
+  
+  REPORT_SERVER: 'https://app1.terra.group/DSDReportViewerSSRS_PHASE2/Default?ID=',
   //REPORT_SERVER: 'http://DB2/ReportServer/',
-  REPORT_URL : 'DSD MnE/',
+  REPORT_URL: 'DSD MnE/',
 
   //PowerBiREPORT_URL: 'https://app.powerbi.com/view?r=eyJrIjoiZTZkMWIzNzQtZWNkYy00MWNlLTk5N2YtMTk2ZGM2ODc1YWNjIiwidCI6IjJkZTliNDMxLTc4ODItNDczZC05YTQwLThjYjg0YzA2ODA5OSJ9'
 
-  PowerBiREPORT_URL: 'https://app.powerbi.com/view?r=eyJrIjoiMGY3MTM0OTktNmVjNS00NDZjLWI1YWYtN2MwZWZjOTI0ZmJlIiwidCI6IjJkZTliNDMxLTc4ODItNDczZC05YTQwLThjYjg0YzA2ODA5OSJ9&pageName=ReportSection' ,
+  PowerBiREPORT_URL: 'https://app.powerbi.com/view?r=eyJrIjoiMGY3MTM0OTktNmVjNS00NDZjLWI1YWYtN2MwZWZjOTI0ZmJlIiwidCI6IjJkZTliNDMxLTc4ODItNDczZC05YTQwLThjYjg0YzA2ODA5OSJ9&pageName=ReportSection',
 
 };
 
